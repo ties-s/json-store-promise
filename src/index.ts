4426abdee40dae51aca01f51543e9077822b6fe8
Ties <ties.schwertasek@gmail.com>
2018-12-05 22:19:05 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,44 @@
+import * as fs from 'fs-extra';
+
+export class JsonStorage {
+
+	private _data: object;
+	public _setup: Promise<any>;
+
+	async set(key: string, value: any){
+		this._data[key] = value;
+		return this.write();
+	}
+
+	async get(key: string) {
+		return Promise.resolve(this._data[key]);
+	}
+
+	async remove(key: string) {
+		delete this._data[key];
+		return this.write();
+	}
+
+	constructor(private fileName: string) {
+		this._setup = this.setup().then(() => this);
+	}
+
+	private async setup() {
+		await fs.ensureFile(this.fileName);
+		
+	}
+
+	private async read() {
+		this._data = await fs.readJson(this.fileName);
+	}
+
+	private async write() {
+		return fs.writeJSON(this.fileName, this._data);
+	}
+
+	public static create(fileName: string): Promise<JsonStorage> {
+		let s = new JsonStorage(fileName);
+		return s._setup;
+	}
+
+}
